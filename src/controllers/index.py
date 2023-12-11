11f13ec5b30c8cd3f069664bba8a2fb78f3878bd
KIX-ITM <anko1kuchi@gmail.com>
2023-12-11 22:19:41 +0900

    add: 入力と結果出力の画面実装

--- src/controllers/index.py
+++ src/controllers/index.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, url_for, redirect, request, abort
 
+from src.controllers.forms import Form
+from src.models.log import AiAnalysisLog
 import settings
 
 app = Flask(__name__, template_folder=settings.TEMPLATE_FOLDER)
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def get_index():
-    return render_template('index.html')
+    form = Form()
+    if request.method == 'POST':
+        image_path = request.form.get('image_path').strip()
+        return render_template('result.html', image_path=image_path)
+    return render_template('index.html', form=form)
 
 
 class WebServer():
+
     def start(self, debug=False):
-        app.run(host="127.0.0.1", port=settings.PORT, debug=debug)
+        app.config['SECRET_KEY'] = settings.SECRET_KEY
+        app.run(host='127.0.0.1', port=settings.PORT, debug=debug)
 
 
 server = WebServer()
